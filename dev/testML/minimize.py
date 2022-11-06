ac2d5f1b10d6fd6d7452a65f6a64f5875e7b0230
Nikunj Gupta <guptanikunj1997@gmail.com>
2022-11-06 03:39:44 -0500

    added SIRVD model

--- dev/testML/minimize.py
+++ dev/testML/minimize.py
@@ -28,7 +28,7 @@ class SIRModel:
         S0 = N-I0-R0
         y0 = S0, I0, R0
         data_info = data[3:8]
-        print(data_info)
+        print("Data info:", data_info)
 
         guess = [float(x) for x in data[8:10]]
         
@@ -98,7 +98,7 @@ class SIRModel:
 
         data_nr, data, t_train, t = self.init(data_info)
 
-        print(methods)
+        print("methods:", methods)
         out = minimize(self.err, params, method=methods, args=(t_train, data, y0, data_info, ),max_nfev=max_nfev)
         beta_fit=out.params['beta'].value
         gamma_fit=out.params['gamma'].value
@@ -132,9 +132,9 @@ class SIRDModel:
 
         guess = [float(x) for x in data[8:11]]
 
-        print(y0)
-        print(data_info)
-        print(guess)
+        print("y0:", y0)
+        print("data_info:", data_info)
+        print("guess:", guess)
         
         return N, y0, guess, data_info
 
@@ -204,6 +204,112 @@ class SIRDModel:
         kappa_fit=out.params['kappa'].value
         fitted_parameters=([beta_fit, gamma_fit, kappa_fit])
         fitted_curve=self.SIRSolve(y0, t, N, beta_fit, gamma_fit, kappa_fit)
+        mae = mean_absolute_error(data_nr, fitted_curve[:][1])
+
+        return out, data_nr, fitted_parameters, fitted_curve, mae, t 
+
+#-------------------SIRVD model----------------------#
+class SIRVDModel:
+
+    def __init__(self):
+        pass
+
+    def load_config(self, config_name):
+
+        with open("data/"+config_name, "r") as f:
+            data = f.readlines()
+        f.close()
+
+        for i in range(14):
+            data[i]=data[i].rstrip("\n")
+        
+        N = float(data[0])
+        I0 = float(data[1])
+        S0 = N-I0
+        y0 = S0, I0, float(data[2]), float(data[3]), float(data[4])
+        data_info = data[5:9]
+
+        guess = [float(x) for x in data[9:14]]
+
+        print("y0:", y0)
+        print("data_info:", data_info)
+        print("guess:", guess)
+        
+        return N, y0, guess, data_info
+
+
+    def init(self, data_info):
+
+        data_nr= pd.read_csv("data/"+data_info[0])[data_info[1]].to_numpy()
+        data_i = np.resize(data_nr,int(data_info[3]))
+        data_nr_d= pd.read_csv("data/"+data_info[0])[data_info[2]].to_numpy()
+        data_d = np.resize(data_nr_d,int(data_info[3]))
+        t_train = np.linspace(0, int(data_info[3]), int(data_info[3])) #time series for the training
+        t = np.linspace(0, int(len(data_nr)), int(len(data_nr))) #timeseries (\days)
+
+        return data_nr, data_i, data_d, t_train, t
+
+
+    #SIR EDO
+    def deriv(self, y0, t, N, beta, gamma, alpha, sigma, delta): 
+
+        S, I, R, V, D = y0
+        #print(y0)
+        
+        dy = np.zeros(5)
+        dy[0] = -beta * S * I / N + sigma * R - alpha * S  #dSdt 
+        dy[1] = beta * S * I / N - gamma * I - delta * I #dIdt 
+        dy[2] = gamma * I - sigma * R #dRdt 
+        dy[3] = alpha * S   #dVdt 
+        dy[4] = delta * I   #dDdt 
+        return dy
+
+    def SIRSolve(self, y0, t, N, beta, gamma, alpha, sigma, delta):
+
+        res = odeint(self.deriv, y0, t, args=(N, beta, gamma, alpha, sigma, delta)) 
+        S, I, R, V, D = res.T
+
+        return S, I, R, V, D
+
+    def err(self, params, t,  data_i, data_d, y0):
+
+
+        beta = params["beta"]
+        gamma = params["gamma"]
+        alpha = params["alpha"]
+        sigma = params["sigma"]
+        delta = params["delta"]
+        N = params["N"]
+
+        S, I, R, V, D  = self.SIRSolve(y0, t, N, beta, gamma, alpha, sigma, delta)
+
+        err = pow(I - data_i,2) + pow(D - data_d,2)
+
+        return err
+
+    def fit(self , config_name, methods='leastsq', max_nfev=100000):
+
+        N, y0, guess, data_info = self.load_config(config_name)
+
+        params = Parameters()
+        params.add('beta',value=guess[0],min=0, max = 10, vary=True)
+        params.add('gamma',value=guess[1],min=0, max=2, vary=True)
+        params.add('alpha',value=guess[2],min=0, max=2, vary=True)
+        params.add('sigma',value=guess[3],min=0, max=2, vary=True)
+        params.add('delta',value=guess[4],min=0, max=2, vary=True)
+        params.add('N', value=N, vary=False)
+
+        data_nr, data_i, data_d, t_train, t = self.init(data_info)
+
+        out = minimize(self.err, params, method=methods, args=(t_train, data_i, data_d, y0, ),max_nfev=max_nfev)
+        
+        beta_fit=out.params['beta'].value
+        gamma_fit=out.params['gamma'].value
+        alpha_fit=out.params['alpha'].value
+        sigma_fit=out.params['sigma'].value
+        delta_fit=out.params['delta'].value
+        fitted_parameters=([beta_fit, gamma_fit, alpha_fit, sigma_fit, delta_fit])
+        fitted_curve=self.SIRSolve(y0, t, N, beta_fit, gamma_fit, alpha_fit, sigma_fit, delta_fit) 
         mae = mean_absolute_error(data_nr, fitted_curve[:][1])
 
         return out, data_nr, fitted_parameters, fitted_curve, mae, t 
